504972c20300b38d43ec96c88c11c56a55677063
Joseph Nweke <josephchristo615@gmail.com>
2024-02-11 23:55:10 +0100

    Adds: Task 3 file and associates

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@
 Defines routes for different API endpoints.
 */
 const AppController = require("../controllers/AppController");
+const UsersController = require("../controllers/UsersController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
+router.post("/users", UsersController.postNew);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const router = require("./routes/index");
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.use(router);
 
 app.listen(port, () => {
